6475df5152095dd4f03f36cb3027d322e74222ca
KeyLimePie <v.asakura@ymail.com>
2015-09-16 15:13:06 +0200

    improved if statement

--- web/contact.php
+++ web/contact.php
@@ -9,55 +9,58 @@
 	
 	if(ISSET($_POST["target"]) AND $_POST["target"] == "contact-form" AND ISSET($_POST["email"]) AND ISSET($_POST["msg"]) AND ISSET($_POST["g-recaptcha-response"]))
 	{
-		if(is_array($_POST["g-recaptcha-response"]) OR is_array($_POST["email"]) OR is_array($_POST["msg"]))
+		if(is_string($_POST["g-recaptcha-response"]) AND is_string($_POST["email"]) AND is_string($_POST["msg"]))
 		{
-			echo CODE_INVALID_FORMAT;			
-			exit;
+			$captcha_response = htmlspecialchars($_POST["g-recaptcha-response"], ENT_QUOTES, "UTF-8");
+			$email = htmlspecialchars($_POST["email"], ENT_QUOTES, "UTF-8");
+			$msg = htmlspecialchars($_POST["msg"], ENT_QUOTES, "UTF-8");
+			
+			$api_params = array(
+					'secret' => GOOGLE_SECRET,
+					'response' => $captcha_response
+			);
+	
+			$ch = curl_init();
+			
+			curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($api_params));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			
+			$response = curl_exec($ch);
+			$response = json_decode($response);
+			curl_close($ch);
+			
+			if(!$response->success)
+			{
+				echo CODE_CAPTCHA_FAILED;
+				exit;
+			}
+			
+			$id = time().'-'.(strlen($email) + strlen($msg));
+			
+			$header = 'From: '.$email."\r\n"."Content-type: text/html; charset=utf-8";
+			$subject = 'Contact form [threekelv.in] #'.$id;
+			
+			date_default_timezone_set('CET');
+			
+			$message = '<b>Form information:</b><br/>';
+			$message .= 'IP address: '.$_SERVER['REMOTE_ADDR'].'<br />';
+			$message .= 'eMail: '.$email.'<br />';
+			$message .= 'Message ID: '.$id.'<br />';
+			$message.='Sent: '.date("l j F Y H:i:s e").'<br /><br /><br />';
+			$message .= $msg;
+			
+			if(mail(EMAIL,$subject,$message,$header))
+				echo CODE_SUCCESS;
+			else
+				echo CODE_TRANSMISSION_FAILED;
 		}
-		$captcha_response = htmlspecialchars($_POST["g-recaptcha-response"], ENT_QUOTES, "UTF-8");
-		$email = htmlspecialchars($_POST["email"], ENT_QUOTES, "UTF-8");
-		$msg = htmlspecialchars($_POST["msg"], ENT_QUOTES, "UTF-8");
-		
-		$api_params = array(
-				'secret' => GOOGLE_SECRET,
-				'response' => $captcha_response
-		);
-
-		$ch = curl_init();
-		
-		curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($api_params));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		
-		$response = curl_exec($ch);
-		$response = json_decode($response);
-		curl_close($ch);
-		
-		if(!$response->success)
-		{
-			echo CODE_CAPTCHA_FAILED;
-			exit;
-		}
-		
-		$id = time().'-'.(strlen($email) + strlen($msg));
-		
-		$header = 'From: '.$email."\r\n"."Content-type: text/html; charset=utf-8";
-		$subject = 'Contact form [threekelv.in] #'.$id;
-		
-		date_default_timezone_set('CET');
-		
-		$message = '<b>Form information:</b><br/>';
-		$message .= 'IP address: '.$_SERVER['REMOTE_ADDR'].'<br />';
-		$message .= 'eMail: '.$email.'<br />';
-		$message .= 'Message ID: '.$id.'<br />';
-		$message.='Sent: '.date("l j F Y H:i:s e").'<br /><br /><br />';
-		$message .= $msg;
-		
-		if(mail(EMAIL,$subject,$message,$header))
-			echo CODE_SUCCESS;
 		else
-			echo CODE_TRANSMISSION_FAILED;
+		{
+			echo CODE_INVALID_FORMAT;
+			exit;	
+		}
 	}
 	else echo CODE_INCOMPLETE_REQUEST;
 ?>
